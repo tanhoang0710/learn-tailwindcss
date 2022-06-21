28daa83430eda52c81c800d9ea0ee9bcebc09cdf
tanhoang0710 <tanhoang0710@gmail.com>
2022-06-21 18:45:12 +0700

    add Featured Mugs - Product Card

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,11 +17,16 @@ module.exports = {
 			keyframes: {
 				slideDown: {
 					"0%": { transform: "translateY(-100%)" },
-					"100%": { transform: "translateY(0%)" },
+					"100%": { transform: "translateY(0)" },
+				},
+				fadeIn: {
+					from: { opacity: 0 },
+					to: { opacity: 1 },
 				},
 			},
 			animation: {
 				slideDown: "slideDown .5s ease-in-out",
+				fadeIn: "fadeIn .5s ease-in-out",
 			},
 			backgroundImage: {
 				"slider-bg": 'url("./img/Header-Pic.jpg")',
